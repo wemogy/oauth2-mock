86a5ce85f013979545deff576868e40c3491cda4
Sebastian Küsters <sebastian@wemogy.com>
2022-10-21 14:14:42 +0200

    server draft

--- README.md
+++ README.md
@@ -1,1 +1,43 @@
 # ![wemogy logo](https://wemogyimages.blob.core.windows.net/logos/wemogy-github-tiny.png) OAuth2 mock
+
+## Get it running
+
+### Install all dependencies
+
+```bash
+yarn install
+```
+
+### Start the servers
+
+```bash
+yarn start
+```
+
+## Configuration
+
+You can set the following environment variables to configure the servers:
+
+| Name | Description | Default |
+|---|---|---|
+| `OAuth2ServerPort` | The port of the OAuth2 server | `3001` |
+| `TokenProviderPort` | The port of the token provider server | `3001` |
+
+## Token Provider server
+
+### Requesting a token
+
+**POST:** `/jwt`
+
+```json
+{
+    "sub": "test123",
+    "aud": "realtime.eu1.spaceblocks.cloud",
+    "ext": {
+        "app_subject_id": "subject",
+        "app_tenant_id": "tenantId",
+        "spaceblocks_tenant_id": "00000000-0000-0000-0000-000000000001",
+        "spaceblocks_stage_id": "e2esc01"
+    }
+}
+```
---
+++ src/index.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import { OAuth2Server } from 'oauth2-mock-server';
+
+const OAuth2ServerPort = parseInt(process.env.OAuth2ServerPort || '3001');
+const TokenProviderPort = parseInt(process.env.TokenProviderPort || '3002');
+
+const server = new OAuth2Server();
+
+// Generate a new RSA key and add it to the keystore
+await server.issuer.keys.generate('RS256');
+
+// Start the server
+await server.start(OAuth2ServerPort, 'localhost');
+
+// API
+const app = express();
+
+// add json middleware
+app.use(express.json());
+
+app.post('/jwt', async function (req, res) {
+  const jwt = await server.issuer.buildToken({
+    scopesOrTransform: (_, payload) => {
+      Object.assign(payload, req.body);
+    }
+  });
+  res.send(jwt);
+});
+
+app.listen(TokenProviderPort);
+
+console.log('Issuer URL:', server.issuer.url); // -> http://localhost:{OAuth2ServerPort}
+console.log('Token Provider URL:', `http://localhost:${TokenProviderPort}`); // -> http://localhost:3000
